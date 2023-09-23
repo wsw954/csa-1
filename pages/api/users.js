71e8821174e8f5d6bd7688fa0eea18e06bd202e4
wsw954 <32828432+wsw954@users.noreply.github.com>
2023-09-23 00:23:51 -0400

    Commit 8: Add API call to /buyers/dashboard to retrieve user info from db. Add starter routes to /pages for all four database collections

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,11 +1,27 @@
 import dbConnect from "/utils/db";
 import { User } from "/models/user";
+import { adminAuth } from "/utils/firebaseAdmin";
 
 export default async function handler(req, res) {
   await dbConnect();
 
   if (req.method === "GET") {
     const { action, username, email } = req.query;
+    // Extract the token from the Authorization header
+    const token = req.headers.authorization?.split("Bearer ")[1];
+
+    if (!token) {
+      // If the token is not provided, return an unauthorized error
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    try {
+      // Verify the token using Firebase Admin SDK
+      await adminAuth.verifyIdToken(token);
+    } catch (error) {
+      // If token verification fails, return an unauthorized error
+      return res.status(401).json({ error: "Unauthorized" });
+    }
 
     switch (action) {
       case "check-username":
@@ -33,7 +49,24 @@ export default async function handler(req, res) {
           res.status(400).json({ error: "Email parameter is required" });
         }
         break;
-
+      case "get-userInfo":
+        if (email) {
+          try {
+            const user = await User.findOne({ email }, "_id username"); // Retrieve only _id and username
+            if (user) {
+              res.json({ _id: user._id, username: user.username });
+            } else {
+              res.status(404).json({ error: "User not found" });
+            }
+          } catch (error) {
+            res
+              .status(500)
+              .json({ error: "Database query failed: " + error.message });
+          }
+        } else {
+          res.status(400).json({ error: "Email parameter is required" });
+        }
+        break;
       default:
         res.status(400).json({ error: "Invalid request" });
         break;
--- pages/buyers/dashboard.js
+++ pages/buyers/dashboard.js
@@ -1,13 +1,42 @@
+import { useState, useEffect } from "react";
 import NavBar from "/components/global/NavBar";
 import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { useAuth } from "/contexts/AuthContext";
-// import { verifyIdToken } from "/utils/firebaseAdmin";
+import axios from "axios";
 
 export default function Dashboard() {
   const { currentUser } = useAuth();
+  const [userInfo, setUserInfo] = useState(null);
+  const [error, setError] = useState(null);
   const router = useRouter();
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      if (currentUser) {
+        try {
+          // Get the ID token of the current user
+          const token = await currentUser.getIdToken();
+
+          // Include the token in the Authorization header
+          const response = await axios.get(
+            `/api/users?action=get-userInfo&email=${currentUser.email}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          setUserInfo(response.data);
+        } catch (err) {
+          setError(err.response?.data?.error || "An error occurred");
+        }
+      }
+    };
+
+    fetchUserInfo();
+  }, [currentUser]);
 
   if (!currentUser) {
     // Redirect to login page or show a message
@@ -23,8 +52,11 @@ export default function Dashboard() {
           <p className={styles.description}>Buyer dashboard</p>
           <p className={styles.description}>Email: {currentUser.email}</p>
           <p className={styles.description}>UID: {currentUser.uid}</p>
+          {userInfo && (
+            <p className={styles.description}>Username: {userInfo.username}</p>
+          )}
+          {error && <p className={styles.error}>Error: {error}</p>}
           <p className={styles.description}></p>
-          <div></div>
         </main>
       </div>
     );
